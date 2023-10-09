a6971f188bcdb60b38e8b02a0fb8242de4dd81b6
Kartbllansh <movolovo93@gmail.com>
2023-10-09 21:31:15 +0300

    TWTG Добавление Spring в проект

---
+++ src/test/java/com/github/Kartbllansh/twtg/TwitterTgTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Kartbllansh.twtg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterTgTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
